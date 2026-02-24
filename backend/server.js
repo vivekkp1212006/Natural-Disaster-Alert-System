53b84120151995ab5de4778c4eb3aac9f36ba1f9
sayooj sr <apple@Apples-MacBook-Pro.local>
2026-02-25 00:17:01 +0530

    port changed to 5001

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const weatherRoutes= require('./routes/weatherRoutes');
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
